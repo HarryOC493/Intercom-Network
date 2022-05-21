8409efe813c24954a8e2ce2a85e7fa73d26d6e47
Harry O'Connor <harryoconnor007@gmail.com>
2022-05-21 23:59:07 +0100

    Testing Sql Connection

--- Client/main.py
+++ Client/main.py
@@ -1,1 +1,14 @@
-placeholder
+from http import server
+import time
+import mysql.connector
+
+mydb = mysql.connector.connect(user='root', password='dev22', server='some-mysql', database='CETSS')
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SELECT * FROM Messages")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+  print(x)
