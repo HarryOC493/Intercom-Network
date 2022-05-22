7e0c1db38d43c5d8ed6087ad40a5f66604ad8b62
Harry O'Connor <harryoconnor007@gmail.com>
2022-05-22 14:05:50 +0100

    Testing time intervals, and checking mysql data return format

--- Client/main.py
+++ Client/main.py
@@ -1,14 +1,30 @@
-from http import server
 import time
+import schedule
 import mysql.connector
 
 mydb = mysql.connector.connect(user='root', password='dev22', host='some-mysql', database='CETSS')
 
 mycursor = mydb.cursor()
 
-mycursor.execute("SELECT * FROM Messages")
-
-myresult = mycursor.fetchall()
 
 for x in myresult:
   print(x)
+
+
+def poll():
+    mycursor.execute("SELECT * FROM LockdownS")
+    myresult = mycursor.fetchall()
+    if myresult == 0:
+        print('No Lockdown in progresss')
+    else:
+        print('Lockdown Found')
+  
+
+
+schedule.every(15).seconds.do(poll)
+  
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
