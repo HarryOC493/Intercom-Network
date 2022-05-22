6faa8ca9ff20ba219a2b5c60b1aacf2b966addbd
Harry O'Connor <harryoconnor007@gmail.com>
2022-05-22 15:32:05 +0100

    Adding Lockdown Audio File and testing audio player libary

--- Client/main.py
+++ Client/main.py
@@ -2,6 +2,8 @@ import time
 import schedule
 import mysql.connector
 
+from playsound import playsound
+
 mydb = mysql.connector.connect(user='root', password='dev22', host='some-mysql', database='CETSS')
 
 mycursor = mydb.cursor(buffered=True)
@@ -10,17 +12,15 @@ mycursor = mydb.cursor(buffered=True)
 def poll():
     myresult = mycursor.execute("SELECT * FROM Lockdowns")
     LockdownCount = mycursor.rowcount
+    
     if LockdownCount != 0:
         #Lockdown Found
         LockdownRows = mycursor.fetchall() 
         for x in LockdownRows:
-            print(x)
-            print(x[0])
-            print(x[2])
             if x[2] == bytearray(b'1'):
                 print('This is a test')
             else:
-                print('This is not a test')
+                playsound('lockdown.mp3a')
     else:
         #No Lockdown found. Contine
         print('Placeholder') 
