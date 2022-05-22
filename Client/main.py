c46a97de87d60abc68b9987c5d57a9d59bf52ea5
root <HarryOC493>
2022-05-22 14:19:24 +0000

    Lockdown check and test check is working

--- Client/main.py
+++ Client/main.py
@@ -4,26 +4,26 @@ import mysql.connector
 
 mydb = mysql.connector.connect(user='root', password='dev22', host='some-mysql', database='CETSS')
 
-mycursor = mydb.cursor()
+mycursor = mydb.cursor(buffered=True)
 
 
 def poll():
     myresult = mycursor.execute("SELECT * FROM Lockdowns")
-
-    if myresult != None:
+    LockdownCount = mycursor.rowcount
+    if LockdownCount != 0:
         #Lockdown Found
-        LockdownRows mycursor.fetchall() 
-        for x in myresult:
-            if LockdownRows[2] == '0x31':
+        LockdownRows = mycursor.fetchall() 
+        for x in LockdownRows:
+            print(x)
+            print(x[0])
+            print(x[2])
+            if x[2] == bytearray(b'1'):
                 print('This is a test')
             else:
                 print('This is not a test')
     else:
         #No Lockdown found. Contine
-
-
-  
-
+        print('Placeholder') 
 
 schedule.every(15).seconds.do(poll)
   
